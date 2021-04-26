e25aa53ad6a4f2f4553617bd22e4b6420d135321
JK-ming <62088241+JK-ming@users.noreply.github.com>
2021-04-26 18:40:26 +0800

    PPP

---
+++ Cilent.java
@@ -0,0 +1,17 @@
+package PPP;
+
+/**
+ * øÕªß∂À≤‚ ‘¿‡
+ */
+public class Cilent {
+    public static void main(String[] args) {
+        Email email,copyEmail;
+        email = new Email();
+        copyEmail = (Email)email.clone();
+        System.out.println("email==copyEmail?");
+        System.out.println(email==copyEmail);
+        System.out.println("email.getAttachment==copyEmail.getAttachment?");
+        System.out.println(email.getAttachment()==copyEmail.getAttachment());
+
+    }
+}
