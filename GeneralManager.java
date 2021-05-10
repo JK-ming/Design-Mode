ea0149394416ca6f3e4da0a7bf20df7fa2fb7bad
JK-ming <62088241+JK-ming@users.noreply.github.com>
2021-05-10 08:55:34 +0000

    Update GeneralManager.java

--- GeneralManager.java
+++ GeneralManager.java
@@ -8,6 +8,11 @@ public class GeneralManager extends Leader {
     public void handleRequest(LeaveRequest request) {
         if (request.getLeaveDays()<30)
         {
+            System.out.printIn("总经理"+name+"审批员工"+request.getLeaveName()+"的请假条，请假天数为"+request.getLeaveDays()+"天。");
+        }
+        else
+            
+        {
             System.out.println("莫非"+request.getLeaveName()+"想辞职，居然请假"+request.getLeaveDays()+"天。");
         }
     }
